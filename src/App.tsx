d3f59f20b305000fc480f8cff790f4c31e8b2e3b
Telmo Sampaio <telmoasampaio@gmail.com>
2022-11-13 21:12:47 +0000

    testing load balancer

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Hello</p>
+      <p className="read-the-docs">Hello Stock</p>
     </div>
   );
 }
